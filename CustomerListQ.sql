92f447fb6629050f711499f2c28464e2c929196b
Ninjay4 <70654108+Ninjay4@users.noreply.github.com>
2024-02-25 00:48:23 -0500

    Add files via upload
    From the SQL Quick Start Guide; Uses the sTunes Database; Chapter 4
    Retrieves a complete list of all First Names, Last Names, Company, Phone, and Emails
    From the customers table

---
+++ CustomerListQ.sql
@@ -0,0 +1,16 @@
+/*
+CREATED BY: Ninjay4
+CREATED ON: 2/25/24
+DESCRIPTION: SQL QuickStart Guide
+	sTunes Database
+	Writing SQL Statements
+	Chapter 4
+	Retrieve a Customer list that contains:
+	First Names
+	Last Names
+	Company
+	Phone
+	And Emails (if available)
+*/
+SELECT FirstName, LastName, Company, Phone, Email
+FROM customers;
