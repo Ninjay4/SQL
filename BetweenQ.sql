3e5c3b67621e8a20ea0f6fed2b81c81a249852a7
Ninjay4 <70654108+Ninjay4@users.noreply.github.com>
2024-03-09 21:40:40 -0500

    Add files via upload

---
+++ BetweenQ.sql
@@ -0,0 +1,23 @@
+/*
+FILE NAME: BetweenQ
+CREATED BY: Ninjay4
+CREATED ON: 3/9/24
+MODIFIED ON: none
+DESCRIPTION:
+SQL QuickStart Guide
+Part II  Chapter 5
+BETWEEN
+Produce a list of invoices with totals between 1.98 and 5.00
+Ordered by the invoice date
+This query selects InvoiceId, InvoiceDate, and total
+From the invoices TABLE
+Where the total amounts are between 1.98 and 5.00
+Ordered by InvoiceDate
+*/
+SELECT
+	InvoiceId AS 'Invoice Number',
+	InvoiceDate AS 'Invoice Date',
+	total AS 'Invoice Total'
+FROM invoices
+WHERE total BETWEEN 1.98 AND 5.00
+ORDER BY InvoiceDate
---
+++ BetweenQ1.sql
@@ -0,0 +1,26 @@
+/*
+FILE NAME: BetweenQ1
+CREATED BY: Ninjay4
+CREATED ON: 3/9/24
+MODIFIED ON: none
+DESCRIPTION:
+SQL QuickStart Guide
+Part II  Chapter 5
+BETWEEN Operator
+What is the highest invoice amount in the dataset
+Of totals between 1.98 and 5.00, ordered by total?
+This query selects InvoiceId and total
+From the invoices table
+Where the totals are between 1.98 and 5.00
+Ordered by totals descending
+Limit 1 shows only the highest amount
+*/
+SELECT
+	InvoiceId AS 'Invoice Number',
+	total AS 'Invoice Total'
+FROM 
+	invoices
+WHERE
+	total BETWEEN 1.98 AND 5.00
+ORDER BY
+	total DESC LIMIT 1
