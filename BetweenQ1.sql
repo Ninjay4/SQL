db66584f33cbcd2941a7fc581aaa8c41e89ca1d2
Ninjay4 <70654108+Ninjay4@users.noreply.github.com>
2024-03-15 23:40:40 -0400

    Update BetweenQ1.sql
    Edit header info

--- BetweenQ1.sql
+++ BetweenQ1.sql
@@ -7,9 +7,12 @@ DESCRIPTION:
 SQL QuickStart Guide
 Use with sTunes Database
 Part II  Chapter 5
-TOPIC: BETWEEN Operator
-TASK: What is the highest invoice amount in the dataset
+TOPIC: 
+BETWEEN Operator
+TASK:
+What is the highest invoice amount in the dataset
 Of totals between 1.98 and 5.00, ordered by total?
+CODE:
 This query selects InvoiceId and total
 From the invoices table
 Where the totals are between 1.98 and 5.00
