3776f44b3d461c7c7d3622294a058300f89bf916
Ninjay4 <70654108+Ninjay4@users.noreply.github.com>
2024-03-16 23:41:33 -0400

    Update COUNTMCIQ.sql
    Edit header info

--- COUNTMCIQ.sql
+++ COUNTMCIQ.sql
@@ -6,8 +6,12 @@ MODIFIED ON: none
 DESCRIPTION:
 SQL QuickStart Guide
 Part II  Chapter 5
+Use with sTunes Database
+TOPIC:
 Filtering Records
+TASK:
 How many invoices were sent to Tucson, Paris, or London?
+CODE:
 This query selects the count of InvoiceId
 From the invoices table 
 Where BillingCity is in the set of {London, Paris, Tucson}
