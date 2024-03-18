1559071b287c07760166b6254dd9d4fb54ccd1ea
Ninjay4 <70654108+Ninjay4@users.noreply.github.com>
2024-03-17 23:05:06 -0400

    Update CountTIQ.sql
    Edit header info

--- CountTIQ.sql
+++ CountTIQ.sql
@@ -5,9 +5,13 @@ CREATED ON: 3/12/24
 MODIFIED ON: none
 DESCRIPTION:
 SQL QuickStart Guide
+Use with sTunes Database
 Part II  Chapter 5
+TOPIC:
 Filter Records
+TASK:
 How many invoices were billed to Tucson?
+CODE:
 This query selects the count of InvoiceId
 From the invoices table 
 Where BillingCity equals Tucson
