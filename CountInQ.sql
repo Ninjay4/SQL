4de5272d464e39019d7a09bdf35eade4740282f0
Ninjay4 <70654108+Ninjay4@users.noreply.github.com>
2024-03-17 23:02:18 -0400

    Update CountInQ.sql
    Edit header info

--- CountInQ.sql
+++ CountInQ.sql
@@ -5,10 +5,14 @@ CREATED ON: 3/10/24
 MODIFIED ON: none
 DESCRIPTION:
 SQL QuickStart Guide
+Use with sTunes Database
 Part II  Chapter 5
+TOPIC:
 IN Operator
+TASK:
 How many records have totals
 That equal either 1.98 or 3.96?
+CODE:
 This query selects the number of totals
 From the invoice table 
 Where total equals either 1.98 or 3.96
