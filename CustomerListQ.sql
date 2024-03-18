2c5e6c9929b64336a6e5c5d36d2b47d7e2f2ce04
Ninjay4 <70654108+Ninjay4@users.noreply.github.com>
2024-03-17 23:08:24 -0400

    Update CustomerListQ.sql
    Edit header info

--- CustomerListQ.sql
+++ CustomerListQ.sql
@@ -1,16 +1,20 @@
 /*
 CREATED BY: Ninjay4
 CREATED ON: 2/25/24
-DESCRIPTION: SQL QuickStart Guide
-	sTunes Database
-	Writing SQL Statements
-	Chapter 4
-	Retrieve a Customer list that contains:
-	First Names
-	Last Names
-	Company
-	Phone
-	And Emails (if available)
+MODIFIED ON: none
+DESCRIPTION:
+SQL QuickStart Guide
+Use with sTunes Database
+Chapter 4
+TOPIC:
+Writing SQL Statements
+TASK:
+Retrieve a Customer list that contains:
+First Names
+Last Names
+Company
+Phone
+And Emails (if available)
 */
 SELECT FirstName, LastName, Company, Phone, Email
 FROM customers;
