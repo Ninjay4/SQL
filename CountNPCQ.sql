c31f2f62d9c5da099bc110e1302a6f889afdb397
Ninjay4 <70654108+Ninjay4@users.noreply.github.com>
2024-03-17 23:03:51 -0400

    Update CountNPCQ.sql
    Edit header info

--- CountNPCQ.sql
+++ CountNPCQ.sql
@@ -5,10 +5,14 @@ CREATED ON: 3/5/24
 MODIFIED ON: none
 DESCRIPTION:
 SQL QuickStart Guide
+Use with sTunes Database
 Part 1 Chapter 4
+TOPIC:
 Data Analysis Checkpoint #3
+TASK:
 How many customers do not have
 A postal code listed?
+CODE:
 Select postal code from the customers table,
 Ordered by postal code ascending.
 Any null values will appear at
