1b56cb0b9a89e8a806e6308b49976fb5f6a3fd8b
Ninjay4 <70654108+Ninjay4@users.noreply.github.com>
2024-03-19 23:51:31 -0400

    Update LikeQ1.sql
    Edit header info

--- LikeQ1.sql
+++ LikeQ1.sql
@@ -5,9 +5,13 @@ CREATED ON: 3/13/24
 MODIFIED ON: none
 DESCRIPTION:
 SQL QuickStart Guide
+Use with sTunes Database
 Part II  Chapter 5
+TOPIC:
 LIKE Operator
+TASK:
 List all invoice that have a 'T' anywhere in its billing City
+CODE:
 This query selects InvoiceId and BillingCity
 From the invoices table 
 Where there is a 'T' anywhere in the billing City
