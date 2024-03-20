b5ff06dc8d4f80d7c02a57d3c19dae609ead886e
Ninjay4 <70654108+Ninjay4@users.noreply.github.com>
2024-03-19 23:52:45 -0400

    Update LikeQ2.sql
    Edit header info

--- LikeQ2.sql
+++ LikeQ2.sql
@@ -5,9 +5,13 @@ CREATED ON: 3/13/24
 MODIFIED ON: none
 DESCRIPTION:
 SQL QuickStart Guide
+Use with sTunes Database
 Part II  Chapter 5
+TOPIC:
 LIKE Operator
+TASK:
 Find all invoices that do NOT have a 'T' in the billing ciy
+CODE:
 This query selects InvoiceId and BillingCity
 From the invoices table 
 Where the billing ciy does NOT have a 'T' in it
