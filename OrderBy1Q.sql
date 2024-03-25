9904080877fc496dab1399b40ce5f9f7600f5e6a
Ninjay4 <70654108+Ninjay4@users.noreply.github.com>
2024-03-24 23:49:01 -0400

    Update OrderBy1Q.sql
    Edit header info

--- OrderBy1Q.sql
+++ OrderBy1Q.sql
@@ -4,8 +4,11 @@ CREATED BY: Ninjay4
 CREATED ON: 3/3/24
 DESCRIPTION: 
 SQL QuickStart Guide
-Chapter 4
+Use with sTunes Database 
+Part II  Chapter 4
+TOPIC:
 Using the ORDER BY Clause
+CODE:
 This query selects the first name,
 last name, and email from the customers table,
 ordered by Last name
